095dd1c5f2a235e028e87095fc9f0a0ff111e6a9
Huy Tran <baohuy82.th@gmail.com>
2025-04-22 13:58:04 -0700

    Update App.jsx

--- taskly/src/App.jsx
+++ taskly/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import Button from "@mui/material/Button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>taskly</div>
