a3ed8d8e0527a93d61b1f3c95650ef3b48925e7a
Huy Tran <baohuy82.th@gmail.com>
2025-04-23 14:13:21 -0700

    Update App.jsx

--- taskly/src/App.jsx
+++ taskly/src/App.jsx
@@ -1,7 +1,11 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 import Button from "@mui/material/Button";
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
 import HomeIcon from "@mui/icons-material/Home";
+import { pink } from "@mui/material/colors";
+import "./App.css";
 
 function App() {
   return (
