bfe459122e12eeba4f164207bc04ff5c524b8d8a
Huy Tran <baohuy82.th@gmail.com>
2025-04-24 11:50:52 -0700

    Update App.jsx

--- taskly/src/App.jsx
+++ taskly/src/App.jsx
@@ -3,11 +3,13 @@ import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
 import HomeIcon from "@mui/icons-material/Home";
 import { pink } from "@mui/material/colors";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <>
       <div>taskly</div>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
