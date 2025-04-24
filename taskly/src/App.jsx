ee5f822b1b1124869476b6cc1effcf94cb7aa337
Huy Tran <baohuy82.th@gmail.com>
2025-04-24 11:46:30 -0700

    Update App.jsx

--- taskly/src/App.jsx
+++ taskly/src/App.jsx
@@ -5,7 +5,6 @@ import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
 import HomeIcon from "@mui/icons-material/Home";
 import { pink } from "@mui/material/colors";
-import "./index.css";
 
 function App() {
   return (
