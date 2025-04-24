d3b85b23b433737dd29186023fe989f234f50c08
Huy Tran <baohuy82.th@gmail.com>
2025-04-24 12:45:19 -0700

    Update theme.js

--- taskly/src/theme.js
+++ taskly/src/theme.js
@@ -1,12 +1,12 @@
-import { red } from "@mui/material/colors";
+import { deepOrange, red } from "@mui/material/colors";
 import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 
 const theme = extendTheme({
   colorSchemes: {
     light: {
       palette: {
-        primary: {
-          main: "#ff5252",
+        primary: teal,
+          secondary: deepOrange,
         },
       },
     },
