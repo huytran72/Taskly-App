11534b9d405858d555e19147e3042ba51b11b4b3
Huy Tran <baohuy82.th@gmail.com>
2025-04-24 12:18:24 -0700

    Update main.jsx

--- taskly/src/main.jsx
+++ taskly/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import { CssBaseline } from "@mui/material";
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
