e2231a6b3f2264ef0300041d75868cd2a87bc536
Huy Tran <baohuy82.th@gmail.com>
2025-04-24 11:13:01 -0700

    Update main.jsx

--- taskly/src/main.jsx
+++ taskly/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
