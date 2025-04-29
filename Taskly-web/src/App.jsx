b03a136997dc230dc6a2dc97cf24c14fcc35c890
Huy Tran <baohuy82.th@gmail.com>
2025-04-28 23:27:32 -0700

    Update App.jsx

--- Taskly-web/src/App.jsx
+++ Taskly-web/src/App.jsx
@@ -4,6 +4,7 @@ import Button from "@mui/material/Button"
 function App() {
   return (
     <>
+      <div>kennytran</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
