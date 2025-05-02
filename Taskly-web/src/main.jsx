dfa13fd89925de34b6b90d5b96c746090dbcf577
Huy Tran <baohuy82.th@gmail.com>
2025-05-01 23:46:40 -0700

    Update main.jsx

--- Taskly-web/src/main.jsx
+++ Taskly-web/src/main.jsx
@@ -10,7 +10,6 @@ createRoot(document.getElementById("root")).render(
     <ThemeProvider theme={theme}>
       <App />
       <CssBaseline />
-      <App />
     </ThemeProvider>
   </StrictMode>
 )
