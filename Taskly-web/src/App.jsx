c9e487d436018e581f78414151b329ffe425e3ed
Huy Tran <baohuy82.th@gmail.com>
2025-05-02 22:22:33 -0700

    Update App.jsx

--- Taskly-web/src/App.jsx
+++ Taskly-web/src/App.jsx
@@ -9,6 +9,9 @@ function App() {
   return (
     <>
       <div>kennytran</div>
+
+      <Typography variant="h1">h1 Heading</Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
