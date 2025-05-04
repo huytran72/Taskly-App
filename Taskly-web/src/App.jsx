e96d6454332620251c44182bcecf3fa161089228
Huy Tran <baohuy82.th@gmail.com>
2025-05-03 22:46:45 -0700

    Update App.jsx

--- Taskly-web/src/App.jsx
+++ Taskly-web/src/App.jsx
@@ -10,6 +10,19 @@ import {
   useColorScheme,
 } from "@mui/material/styles"
 
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === "light" ? "dark" : "light")
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  )
+}
+
 function App() {
   return (
     <>
