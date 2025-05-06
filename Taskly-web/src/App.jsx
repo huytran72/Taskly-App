35830ef3fcbaa37174f15c7ba62a59ad5675a1f8
Huy Tran <baohuy82.th@gmail.com>
2025-05-05 23:53:56 -0700

    Update App.jsx

--- Taskly-web/src/App.jsx
+++ Taskly-web/src/App.jsx
@@ -6,6 +6,35 @@ import { pink } from "@mui/material/colors"
 import Typography from "@mui/material/Typography"
 import { useColorScheme } from "@mui/material/styles"
 
+
+export default function BasicSelect() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
+
+
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
   return (
