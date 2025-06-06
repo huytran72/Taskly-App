2c3c73ca729254ba113c78524ebdb1a9b7132f0f
Huy Tran <baohuy82.th@gmail.com>
2025-06-05 22:58:47 -0700

    Update App.jsx

--- Taskly-web/src/App.jsx
+++ Taskly-web/src/App.jsx
@@ -28,7 +28,7 @@ function ModeSelect() {
         label="Mode"
         onChange={handleChange}
       >
-        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value="light">Ten</MenuItem>
         <MenuItem value={20}>Twenty</MenuItem>
         <MenuItem value={30}>Thirty</MenuItem>
       </Select>
