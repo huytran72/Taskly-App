91b06b8aa4dea87f4bf0984691467644eac420ee
Huy Tran <baohuy82.th@gmail.com>
2025-06-22 15:47:13 -0700

    Update main.jsx

--- Taskly-web/src/main.jsx
+++ Taskly-web/src/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 import CssBaseline from "@mui/material/CssBaseline"
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles"
-import { ThemeProvider } from "@mui/material/styles"
-import theme from "./theme.js"
+import theme from "./theme"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
