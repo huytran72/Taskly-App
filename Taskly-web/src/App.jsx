3931eb758c20634e79718f591880283f94433f68
Huy Tran <baohuy82.th@gmail.com>
2025-06-29 22:53:16 -0700

    Update App.jsx

--- Taskly-web/src/App.jsx
+++ Taskly-web/src/App.jsx
@@ -1,19 +1,4 @@
-import Button from "@mui/material/Button"
-import AccessAlarmIcon from "@mui/icons-material/AccessAlarm"
-import ThreeDRotation from "@mui/icons-material/ThreeDRotation"
-import HomeIcon from "@mui/icons-material/Home"
-import { pink } from "@mui/material/colors"
-import Typography from "@mui/material/Typography"
-import { useColorScheme } from "@mui/material/styles"
-import InputLabel from "@mui/material/InputLabel"
-import MenuItem from "@mui/material/MenuItem"
-import FormControl from "@mui/material/FormControl"
-import Select from "@mui/material/Select"
-import Box from "@mui/material/Box"
-import LightModeIcon from "@mui/icons-material/LightMode"
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined"
-import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness"
-import Container from "@mui/material/Container"
+import Board from "./pages/Boards/_id"
 
 function App() {
   return <></>
