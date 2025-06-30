515c2bccd0a0a90edc15043f6c9bb2f708be2333
Huy Tran <baohuy82.th@gmail.com>
2025-06-29 22:51:18 -0700

    Update _id.jsx

--- Taskly-web/src/pages/Boards/_id.jsx
+++ Taskly-web/src/pages/Boards/_id.jsx
@@ -1,7 +1,47 @@
 import React from "react"
 
 function Board() {
-  return <div>_id</div>
+  return (
+    <Container
+      disableGutters
+      maxWidth={false}
+      sx={{ height: "100vh", backgroundColor: "primary.main" }}
+    >
+      <Box
+        sx={{
+          backgroundColor: "primary.light",
+          width: "100%",
+          height: "48px",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <ModeSelect />
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.dark",
+          width: "100%",
+          height: "58px",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        Board Bar
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+          width: "100%",
+          height: "calc(100vh - 106px)",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        Board Content
+      </Box>
+    </Container>
+  )
 }
 
 export default Board
