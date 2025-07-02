11bac9507cbbca36aad13587764812c72392ca80
Huy Tran <baohuy82.th@gmail.com>
2025-07-02 15:35:03 -0700

    Update index.jsx

--- Taskly-web/src/pages/Boards/BoardBar/index.jsx
+++ Taskly-web/src/pages/Boards/BoardBar/index.jsx
@@ -1,7 +1,19 @@
 import React from "react"
 
 function BoardBar() {
-  return <div>BoardBar() </div>
+  return (
+    <Box
+      sx={{
+        backgroundColor: "primary.dark",
+        width: "100%",
+        height: "58px",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      Board Bar
+    </Box>
+  )
 }
 
 export default BoardBar
