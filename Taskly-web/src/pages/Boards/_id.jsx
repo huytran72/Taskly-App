60a637d05ccf3792f8c209c992466459575fd8d9
Huy Tran <baohuy82.th@gmail.com>
2025-07-03 22:46:33 -0700

    Update _id.jsx

--- Taskly-web/src/pages/Boards/_id.jsx
+++ Taskly-web/src/pages/Boards/_id.jsx
@@ -1,7 +1,4 @@
-import React from "react"
 import Container from "@mui/material/Container"
-import Box from "@mui/material/Box"
-import ModeSelect from "../../components/ModeSelect"
 import AppBar from "../../components/AppBar"
 import BoardBar from "./BoardBar"
 import BoardContent from "./BoardContent"
