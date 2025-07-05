9a90d2edfe81d06647dae06c2ee2f51cbe127079
Huy Tran <baohuy82.th@gmail.com>
2025-07-04 23:08:07 -0700

    Update index.jsx

--- Taskly-web/src/components/ModeSelect/index.jsx
+++ Taskly-web/src/components/ModeSelect/index.jsx
@@ -1,5 +1,3 @@
-import React from "react"
-import Button from "@mui/material/Button"
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm"
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation"
 import HomeIcon from "@mui/icons-material/Home"
