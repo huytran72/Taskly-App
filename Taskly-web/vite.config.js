81d639b9d9f4b95624f11ed0edbb1a23a9dab00b
Huy Tran <baohuy82.th@gmail.com>
2025-07-07 10:34:21 -0700

    Update vite.config.js

--- Taskly-web/vite.config.js
+++ Taskly-web/vite.config.js
@@ -4,7 +4,7 @@ import svgr from "vite-plugin-svgr"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: "~", replacement: "/src" }],
   },
