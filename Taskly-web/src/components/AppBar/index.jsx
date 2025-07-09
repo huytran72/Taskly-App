27b9ec9c2f7fb43062732d24beffe559751b82b9
Huy Tran <baohuy82.th@gmail.com>
2025-07-09 15:12:55 -0700

    Update index.jsx

--- Taskly-web/src/components/AppBar/index.jsx
+++ Taskly-web/src/components/AppBar/index.jsx
@@ -3,6 +3,7 @@ import ModeSelect from "~/components/ModeSelect"
 import AppsIcon from "@mui/icons-material/Apps"
 import { ReactComponent as TasklyIcon } from "~/assets/taskly.svg"
 import SvgIcon from "@mui/material/SvgIcon"
+import { Typography } from "@mui/material"
 
 function AppBar() {
   return (
@@ -23,6 +24,7 @@ function AppBar() {
           inheritViewBox
           sx={{ color: "primary.main" }}
         />
+        <Typography>Taskly</Typography>
       </Box>
       <Box>
         <ModeSelect />
