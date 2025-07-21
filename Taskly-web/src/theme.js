03d0ad8fed4b92d2e10247867978b5f136980b17
Huy Tran <baohuy82.th@gmail.com>
2025-07-20 23:53:51 -0700

    Update theme.js

--- Taskly-web/src/theme.js
+++ Taskly-web/src/theme.js
@@ -26,7 +26,10 @@ const theme = extendTheme({
   components: {
     MuiButton: {
       styleOverrides: {
-        root: () => ({}),
+        root: (theme) => ({
+          color: theme.palette.primary.main,
+          fontSize: "0.875rem", // 14px
+        }),
       },
     },
     MuiInputLabel: {
