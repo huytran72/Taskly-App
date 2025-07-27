d810f41937ee499e0379322b1309ad2c956f2af2
Huy Tran <baohuy82.th@gmail.com>
2025-07-26 23:57:17 -0700

    Update index.jsx

--- Taskly-web/src/pages/Boards/BoardBar/index.jsx
+++ Taskly-web/src/pages/Boards/BoardBar/index.jsx
@@ -17,11 +17,7 @@ function BoardBar() {
       }}
     >
       <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}> </Box>
-      <Chip
-        icon={<DashboardIcon />}
-        label="KennyTran Board"
-        onClick={() => {}}
-      />
+      <Chip icon={<DashboardIcon />} label="KennyTran Board" clickable />
       <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}> </Box>
     </Box>
   )
