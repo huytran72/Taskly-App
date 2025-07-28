00396a0dc9ffceb378553bdcb8828e40adcd96a8
Huy Tran <baohuy82.th@gmail.com>
2025-07-27 23:57:44 -0700

    Update index.jsx

--- Taskly-web/src/pages/Boards/BoardBar/index.jsx
+++ Taskly-web/src/pages/Boards/BoardBar/index.jsx
@@ -69,7 +69,7 @@ function BoardBar() {
           },
         }}
         icon={<VpnLockIcon />}
-        label="Public/Private Workspace"
+        label="Add To Google Drive"
         clickable
       />
       <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}> </Box>
