e27392faa43186311252a05581ff0d5fa81e2dc5
Huy Tran <baohuy82.th@gmail.com>
2025-08-07 23:59:34 -0700

    Update index.jsx

--- Taskly-web/src/pages/Boards/BoardBar/index.jsx
+++ Taskly-web/src/pages/Boards/BoardBar/index.jsx
@@ -30,8 +30,9 @@ function BoardBar() {
         height: (theme) => theme.taskly.boardBarHeight,
         display: "flex",
         alignItems: "center",
-        justifyContent: "center-between",
+        justifyContent: "space-between",
         gap: 2,
+        paddingX: 2,
         overflowX: "auto",
         borderTop: "1px solid #00bfa5",
       }}
