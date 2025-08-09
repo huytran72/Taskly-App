37e66e73652c85afb254f893864526b53409d2e5
Huy Tran <baohuy82.th@gmail.com>
2025-08-08 21:14:29 -0700

    Update index.jsx

--- Taskly-web/src/pages/Boards/BoardBar/index.jsx
+++ Taskly-web/src/pages/Boards/BoardBar/index.jsx
@@ -74,7 +74,6 @@ function BoardBar() {
         <AvatarGroup max={4} total={10}>
           <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
           <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
         </AvatarGroup>
       </Box>
     </Box>
