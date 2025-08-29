d8f8ed1ba755fa302fe0d389978c9b3cb2d09c45
Huy Tran <baohuy82.th@gmail.com>
2025-08-28 23:56:13 -0700

    Update index.jsx

--- Taskly-web/src/components/ModeSelect/index.jsx
+++ Taskly-web/src/components/ModeSelect/index.jsx
@@ -19,7 +19,12 @@ function ModeSelect() {
 
   return (
     <FormControl size="small" sx={{ minWidth: "120px" }}>
-      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <InputLabel
+        id="label-select-dark-light-mode"
+        sx={{ color: "white", "&.Mui-focused": { color: "white" } }}
+      >
+        Mode
+      </InputLabel>
       <Select
         labelId="label-select-dark-light-mode"
         id="select-dark-light-mode"
