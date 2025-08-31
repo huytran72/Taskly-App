b8a2bc84b66d734cfe518b9b37887e3872cc257f
Huy Tran <baohuy82.th@gmail.com>
2025-08-30 23:05:14 -0700

    Update index.jsx

--- Taskly-web/src/pages/Boards/BoardBar/index.jsx
+++ Taskly-web/src/pages/Boards/BoardBar/index.jsx
@@ -96,7 +96,7 @@ function BoardBar() {
               width: 30,
               height: 30,
               fontSize: "16px",
-              border: "none ",
+              border: "none",
             },
           }}
         >
