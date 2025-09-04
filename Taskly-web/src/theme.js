3904538bed772b7ae722d462db1884a474140ec0
Huy Tran <baohuy82.th@gmail.com>
2025-09-03 22:42:09 -0700

    Update theme.js

--- Taskly-web/src/theme.js
+++ Taskly-web/src/theme.js
@@ -4,8 +4,7 @@ import { Height } from "@mui/icons-material"
 
 const APP_BAR_HEIGHT = "58px"
 const BOARD_BAR_HEIGHT = "60px"
-const BOARD_CONTENT_HEIGHT =
-  "calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})"
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
 
 // Create a theme instance.
 const theme = extendTheme({
