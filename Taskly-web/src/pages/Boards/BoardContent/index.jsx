768c0ab3ecd17d27949a43e30f3cfece52063fe8
Huy Tran <baohuy82.th@gmail.com>
2025-09-03 22:49:42 -0700

    Update index.jsx

--- Taskly-web/src/pages/Boards/BoardContent/index.jsx
+++ Taskly-web/src/pages/Boards/BoardContent/index.jsx
@@ -1,11 +1,31 @@
 import React from "react"
 import Box from "@mui/material/Box"
 import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+import Menu from "@mui/material/Menu"
+import MenuItem from "@mui/material/MenuItem"
+import Divider from "@mui/material/Divider"
+import ListItemText from "@mui/material/ListItemText"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ContentCut from "@mui/icons-material/ContentCut"
+import ContentCopy from "@mui/icons-material/ContentCopy"
+import ContentPaste from "@mui/icons-material/ContentPaste"
+import Cloud from "@mui/icons-material/Cloud"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 
 const COLUMN_HEADER_HEIGHT = "48px"
 const COLUMN_FOOTER_HEIGHT = "48px"
 
 function BoardContent() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <Box
       sx={{
@@ -40,8 +60,66 @@ function BoardContent() {
           }}
         >
           <Typography variant="h6">Header</Typography>
+          <Box>
+            <Button
+              id="basic-button-workspaces"
+              aria-controls={open ? "basic-menu-workspaces" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+              endIcon={<ExpandMoreIcon />}
+            >
+              Workspaces
+            </Button>
+            <Menu
+              id="basic-menu-workspaces"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button-workspaces",
+              }}
+            >
+              <MenuItem>
+                <ListItemIcon>
+                  <ContentCut fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Cut</ListItemText>
+                <Typography variant="body2" color="text.secondary">
+                  ⌘X
+                </Typography>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <ContentCopy fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Copy</ListItemText>
+                <Typography variant="body2" color="text.secondary">
+                  ⌘C
+                </Typography>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <ContentPaste fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Paste</ListItemText>
+                <Typography variant="body2" color="text.secondary">
+                  ⌘V
+                </Typography>
+              </MenuItem>
+              <Divider />
+              <MenuItem>
+                <ListItemIcon>
+                  <Cloud fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Web Clipboard</ListItemText>
+              </MenuItem>
+            </Menu>
+          </Box>
         </Box>
+
         <Box sx={{}}>List Cards</Box>
+
         <Box
           sx={{
             height: COLUMN_FOOTER_HEIGHT,
